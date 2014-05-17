218d6cf37401d10194c817c70bb0c310e2fcc853
Tom Chan <locpk123@gmail.com>
2014-05-18 03:13:30 +0800

    last commit missed some files

---
+++ WebContent/insert.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+$('#book').submit(function(){
+var cus_id= $('#cus_id').val(); 
+var cus_name= $('#cus_name').val();
+var cus_phone= $('#cus_phone').val();
+var roomtype= $('#roomtype').val();
+var cus_num= $('#cus_num').val();
+var from= $('#from').val();
+var to= $('#to').val();
+var cus_message= $('#cus_message').val();
+$.ajax({type:"get",url:"book.jsp",data:"cus_id="
++cus_id+"cus_name="+cus_name+"cus_phone="+cus_phone+"roomtype="+roomtype+"cus_num="+cus_num+"from="+from+"to="+to+"cus_message="+cus_message,success:function(){$('#result').hide(); 
+    $("#result").html("<h1>You booked successfully!</h1>").fadeIn("slow"); } });
+return false;
+});
+});
